924efb0f25a6cf8dd3c6131328969ed2a444d671
Narendra <nkvidda@gmail.com>
2024-01-27 11:30:43 +0530

    tailwindcss and vite frontend setup

---
+++ webapp/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-950 text-white p-2 rounded-lg text-base'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
